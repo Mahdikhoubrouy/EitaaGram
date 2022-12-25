1747237a50270fd9af808e0997537ab530dc8a8d
Mahdi Khoubrouy <mahdikhoubrouy@gmail.com>
2022-12-25 19:06:01 +0330

    Fixed

---
+++ EitaaGram.BotLib/BotClient/BotInformation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EitaaGram.BotLib.BotClient
+{
+    internal static class BotInformation
+    {
+        private static string Token;
+
+        public static void SetToken(string token)
+        {
+            Token = token;
+        }
+
+        public static string GetToken()
+        {
+            return Token;
+        }
+
+        public static string GetAPIURL(string Token, string Method)
+        {
+            return $"https://eitaayar.ir/api/{Token}/{Method}";
+        }
+
+    }
+}
---
+++ EitaaGram.BotLib/BotClient/EitaaGramBotClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EitaaGram.BotLib.BotClient
+{
+    public class EitaaGramBotClient
+    {
+        public EitaaGramBotClient(string Token)
+        {
+            BotInformation.SetToken(Token);
+        }
+
+
+    }
+}
