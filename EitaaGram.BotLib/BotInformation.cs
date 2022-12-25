8f2d43c35d5f1888832473ad4cd5aac9827eba96
Mahdi Khoubrouy <mahdikhoubrouy@gmail.com>
2022-12-25 19:00:39 +0330

    Add project files.

---
+++ EitaaGram.BotLib/BotInformation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EitaaGram.BotLib
+{
+    internal class BotInformation
+    {
+        public readonly string Token;
+    }
+}
---
+++ EitaaGram.BotLib/EitaaGramBotClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EitaaGram.BotLib
+{
+    public class EitaaGramBotClient
+    { 
+    }
+}
